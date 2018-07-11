d4c839712f0d06c97f086df9b9ede1e77f868ba8
Sreepriya Sreekumar <sreepriyaks@gmail.com>
2018-07-11 23:53:38 +0530

    minor

--- generateTxs.js
+++ generateTxs.js
@@ -3,12 +3,18 @@ const makeRequest = require('./common').makeRequest;
 
 
 var i, key, value;
-for (i = 0; i < 5; i++) {
+for (i = 0; i < 1000; i++) {
     key = `Key${getRandomInt(77437)}`;
     value = `Value${getRandomInt(26346)}`;
     tx = `${key}=${value}`;
     console.log(`${config.tx_baseUrl}"${tx}"`);
     makeRequest(`${config.tx_baseUrl}"${tx}"`)
+    .then(result => {
+        console.log(result);
+    })
+    .catch(err => {
+        console.log(err);
+    })
 }
 
 function getRandomInt(max) {
